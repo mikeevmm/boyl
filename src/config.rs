ccae7499218db36adb07b31298819be191f09509
mikeevmm <miguelmurca@gmail.com>
2021-06-10 01:29:52 +0100

    feat: configuration file loading and serializing working

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,130 @@
-pub fn default_config_dir() -> &'static str {
-    todo!()
+use serde::{Deserialize, Serialize};
+use std::{
+    fmt::Display,
+    fs,
+    io::{BufReader, BufWriter},
+    path::{Path, PathBuf},
+};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Config {
+    templates: Vec<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config { templates: vec![] }
+    }
+}
+
+pub enum LoadConfigError {
+    NotAFile(String),
+    FileError(std::io::Error),
+    BadDeserialization(serde_json::Error, String),
+}
+
+impl Display for LoadConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LoadConfigError::NotAFile(path) => write!(
+                f,
+                "Configuration JSON path ({}) exists, but is not a file!",
+                path
+            ),
+            LoadConfigError::FileError(e) => {
+                write!(
+                    f,
+                    "Error opening the configuration JSON file for reading: {}",
+                    e
+                )
+            }
+            LoadConfigError::BadDeserialization(e, path) => {
+                write!(
+                    f,
+                    "Error parsing the configuration JSON file: {}\n\
+                    You can attempt to fix the file manually, or delete it \
+                    (you will lose your configuration).\n\
+                    The configuration file can be found in '{}'",
+                    e, path
+                )
+            }
+        }
+    }
+}
+
+pub enum WriteConfigError {
+    NotAFile(String),
+    FileError(std::io::Error),
+    BadSerialization(serde_json::Error, String),
+}
+
+impl Display for WriteConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WriteConfigError::NotAFile(path) => write!(
+                f,
+                "Configuration JSON path ('{}') exists, but is not a file!",
+                path
+            ),
+            WriteConfigError::FileError(e) => write!(
+                f,
+                "Error opening the configuration JSON file for writing: '{}'",
+                e
+            ),
+            WriteConfigError::BadSerialization(e, path) => {
+                write!(
+                    f,
+                    "Error writing the memory configurations to file: '{}'\n\
+                    This session's changes have not been saved, and it is \
+                    possible that your configuration file has become corrupted. \
+                    You can attempt to fix the file manually, or delete it \
+                    (you will lose your configuration).\n\
+                    The configuration file can be found in '{}'.",
+                    e, path
+                )
+            }
+        }
+    }
+}
+
+fn get_json_path(config_path: &Path) -> PathBuf {
+    config_path.join("config.json")
+}
+
+pub fn load_config(config_path: &Path) -> Result<Option<Config>, LoadConfigError> {
+    let json_path = get_json_path(config_path);
+    if !json_path.exists() {
+        return Ok(None);
+    }
+    if !json_path.is_file() {
+        return Err(LoadConfigError::NotAFile(
+            json_path.to_string_lossy().to_string(),
+        ));
+    }
+    let json_file = match fs::File::open(json_path.clone()) {
+        Ok(f) => f,
+        Err(x) => return Err(LoadConfigError::FileError(x)),
+    };
+    let reader = BufReader::new(json_file);
+    serde_json::from_reader::<_, Config>(reader)
+        .map_err(|e| {
+            LoadConfigError::BadDeserialization(e, json_path.to_string_lossy().to_string())
+        })
+        .map(Some)
+}
+
+pub fn write_config(config: Config, config_path: &Path) -> Result<(), WriteConfigError> {
+    let json_path = get_json_path(config_path);
+    if json_path.exists() && !json_path.is_file() {
+        return Err(WriteConfigError::NotAFile(
+            json_path.to_string_lossy().to_string(),
+        ));
+    }
+    let json_file = match fs::File::create(json_path.clone()) {
+        Ok(f) => f,
+        Err(e) => return Err(WriteConfigError::FileError(e)),
+    };
+    let writer = BufWriter::new(json_file);
+    serde_json::to_writer(writer, &config)
+        .map_err(|e| WriteConfigError::BadSerialization(e, json_path.to_string_lossy().to_string()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,73 @@
-use std::{fs, path};
+#[macro_use]
+extern crate serde;
+extern crate clap;
+extern crate dirs;
+extern crate num_traits;
+extern crate serde_json;
+extern crate shellexpand;
 
 use clap::{App, AppSettings, Arg};
+use num_traits::PrimInt;
+use std::{
+    io,
+    path::{self, PathBuf},
+};
 
-use crate::verbosity::Verbosity;
-
-mod verbosity;
 mod config;
 
-fn path_exists(path: &str) -> bool {
-    path::Path::new(&shellexpand::tilde(path).to_owned().to_string())
-        .canonicalize()
-        .map_or(false, |p| p.exists())
+#[derive(Debug)]
+enum Verbosity {
+    None,
+    Some,
+    Very,
+}
+
+impl<X> From<X> for Verbosity
+where
+    X: PrimInt,
+{
+    fn from(value: X) -> Self {
+        if value.lt(&X::from(1).unwrap()) {
+            Verbosity::None
+        } else if value.lt(&X::from(2).unwrap()) {
+            Verbosity::Some
+        } else {
+            Verbosity::Very
+        }
+    }
+}
+
+/// Converts a user specified path (potentially using ~) to a
+/// canonicalized PathBuf.
+///
+/// This function returns a `Result` as the `canonicalized()`
+/// call can fail if, for example, the given path does not
+/// exist.
+fn user_path_to_path(path: &str) -> io::Result<PathBuf> {
+    path::Path::new(&shellexpand::tilde(path).to_owned().to_string()).canonicalize()
+}
+
+/// Checks whether a user specified path (potentially using ~)
+/// exists.
+fn user_path_exists(path: &str) -> bool {
+    user_path_to_path(path).map_or(false, |p| p.exists())
+}
+
+/// Gets the default directory for boyl's configuration files,
+/// namely `(default config directory)/boyl`, where the default
+/// configuration directory is given by the `dirs` crate.
+///
+/// As a side effect of this function, **if the default directory
+/// does not exist, it will be created**.
+fn default_config_dir() -> PathBuf {
+    let default_dir = dirs::config_dir()
+        .expect("`dirs` crate does not specify a config directory for this OS.")
+        .join("boyl");
+    if !default_dir.exists() {
+        std::fs::create_dir_all(default_dir.clone())
+            .expect("Failed to create the default configuration directory.");
+    }
+    default_dir
 }
 
 fn main() {
@@ -27,13 +84,25 @@ fn main() {
                 .global(true),
         )
         .arg(
-            Arg::with_name("directory")
-            .hidden(true)
-            .env("BOYL_DIRECTORY")
-            .default_value(config::default_config_dir())
+            Arg::with_name("config_dir")
+                .hidden(true)
+                .env("BOYL_CONFIG")
+                .validator(|dir_str| {
+                    if user_path_exists(&dir_str) {
+                        Ok(())
+                    } else {
+                        Err(format!(
+                            "The specified configuration directory (\"{}\") does not exist.",
+                            &dir_str
+                        ))
+                    }
+                }),
         )
         .subcommand(App::new("list").about("Lists the available projects."))
-        .subcommand(App::new("make").about("Generates a new template from the current folder."))
+        .subcommand(
+            App::new("make")
+                .about("Interactively generates a new template from the current folder."),
+        )
         .subcommand(
             App::new("new")
                 .about("Creates a new boilerplate project.")
@@ -67,7 +136,7 @@ fn main() {
                             folder will be created for the project.",
                         )
                         .validator(|arg_str| {
-                            if path_exists(&arg_str) {
+                            if user_path_exists(&arg_str) {
                                 Ok(())
                             } else {
                                 Err(format!("{} does not exist.", arg_str))
@@ -79,11 +148,29 @@ fn main() {
         .get_matches();
 
     let verbosity = Verbosity::from(matches.occurrences_of("v"));
+    let config_dir = matches
+        .value_of("config_dir")
+        .map_or_else(default_config_dir, |user_path| {
+            user_path_to_path(user_path).unwrap()
+        });
+
+    let config = match config::load_config(&config_dir) {
+        Ok(config) => config.unwrap_or_else(config::Config::default),
+        Err(err) => {
+            clap::Error::with_description(&err.to_string(), clap::ErrorKind::InvalidValue).exit()
+        }
+    };
+
+    println!("{:?}", config);
 
     match matches.subcommand() {
         ("new", Some(sub_matches)) => {}
         ("create", Some(sub_matches)) => {}
         ("list", Some(sub_matches)) => {}
         (name, _) => panic!("Unimplemented subcommand {}", name),
+    }
+
+    if let Err(err) = config::write_config(config, &config_dir) {
+        clap::Error::with_description(&err.to_string(), clap::ErrorKind::InvalidValue).exit()
     }
 }
